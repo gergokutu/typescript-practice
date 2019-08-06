531ae6dbb97b97725eb28b8d44e702680ae677da
gergokutu <gergo.kutu@gmail.com>
2019-08-06 12:00:09 +0200

    @Get and @Post implemented

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,21 +1,38 @@
 // src/pages/controller.ts
-import { JsonController, Get, Param } from 'routing-controllers'
+import { JsonController, Get, Param, Put, Body, Post, HttpCode } from 'routing-controllers'
 import pagesById, { Page, PageList } from './data'
 
 @JsonController()
 export default class PageController {
 
-    @Get('/pages/:id')
-    getPage(
-        @Param('id') id: number
-    ): Page {
-        return pagesById[id]
-    }
+  @Get('/pages/:id')
+  getPage(
+    @Param('id') id: number
+  ): Page {
+    return pagesById[id]
+  }
 
-    @Get('/pages')
-    allPages(): PageList {
-        return {pages: Object.values(pagesById)}
-    }
+  @Get('/pages')
+  allPages(): PageList {
+    return { pages: Object.values(pagesById) }
+  }
 
-    // ..
+  @Put('/pages/:id')
+  updatePage(
+    @Param('id') id: number,
+    @Body() body: Partial<Page>
+  ): Page {
+    console.log(`Incoming PUT body param:`, body)
+    return pagesById[id]
+  }
+
+  @Post('/pages')
+  @HttpCode(201)
+  createPage(
+    @Body() body: Page
+  ): Page {
+    console.log(`Incoming POST body param:`, body)
+    return body
+  }
+
 }
